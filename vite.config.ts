4a2017ee97344d37b7d977840a618a967d415a54
springboot20 <abbasopeyemi148@gmail.com>
2025-07-01 22:17:16 +0100

    seo: install a perfec solution to sitemap generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import sitemap from "vite-sitemap";
 
 import * as path from "path";
 import * as url from "url";
@@ -9,12 +10,20 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  preview: {
-    port: 3006,
-    open: true,
+  plugins: [
+    react(),
+    sitemap({
+      baseURL:
+        process.env.NODE_ENV === "production"
+          ? process.env.VITE_SITEMAP_PROD
+          : process.env.VITE_SITEMAP_DEV,
+    }) as any, // Cast to any to avoid type issues with vite-sitemap
+  ],
+  server: {
+    host: "localhost",
+    port: 3001,
   },
-   resolve: {
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
