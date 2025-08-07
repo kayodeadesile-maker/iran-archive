47bc825581ccb52a03f5cc310b5b8b2b219b1d8c
DESKTOP-8AR3IAJ\USER PC <busariroqeeb16@gmail.com>
2025-08-07 06:34:22 +0100

    test

--- src/pages/explore-archive/plants/plants.tsx
+++ src/pages/explore-archive/plants/plants.tsx
@@ -1,7 +1,7 @@
 const Plants = () => {
   return (
     <section className="pt-10 px-4">
-      <div className="max-w-6xl mx-auto">Plant</div>
+      <div className="max-w-6xl mx-auto">Plants</div>
     </section>
   );
 };
