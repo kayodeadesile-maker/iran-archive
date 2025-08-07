93e0f60e9d0931e8ef66e68b04df6b6e66454a47
DESKTOP-8AR3IAJ\USER PC <busariroqeeb16@gmail.com>
2025-08-08 00:20:20 +0100

    added heroes categories and images or heroes

--- src/pages/explore-archive/heroes-and-legends/heroes-and-legends.tsx
+++ src/pages/explore-archive/heroes-and-legends/heroes-and-legends.tsx
@@ -1,4 +1,56 @@
-import heroesCoverImage from "@/assets/heroes-and-legend-cover-image.jpg"
+import heroesCoverImage from "@/assets/heroes-and-legend-cover-image.jpg";
+import monarchIcon from "@/assets/svgs/monarch-icon.svg";
+import warriorsIcon from "@/assets/svgs/warriors-icon.svg";
+import heroesIcon from "@/assets/svgs/heroes-icon.svg";
+import legendsIcon from "@/assets/svgs/legends-icon.svg";
+import iconsIcon from "@/assets/svgs/icons-icon.svg";
+
+const HEROES_CATEGORIES: HeroesCategoryProps[] = [
+  {
+    categoryName: "Monarchs",
+    iconSrc: monarchIcon,
+    altName: "Monarchs",
+  },
+  {
+    categoryName: "Warriors",
+    iconSrc: warriorsIcon,
+    altName: "Warriors",
+  },
+  {
+    categoryName: "Heroes",
+    iconSrc: heroesIcon,
+    altName: "Heroes",
+  },
+  {
+    categoryName: "Legends",
+    iconSrc: legendsIcon,
+    altName: "Legends",
+  },
+  {
+    categoryName: "Icons",
+    iconSrc: iconsIcon,
+    altName: "Icons",
+  },
+];
+
+interface HeroesCategoryProps {
+  categoryName: string;
+  iconSrc: string;
+  altName?: string;
+}
+
+const HeroesCategory = ({
+  categoryName,
+  iconSrc,
+  altName = categoryName,
+}: HeroesCategoryProps) => {
+  return (
+    <button className="flex flex-col items-center space-x-2 bg-lightgoldcolorfive w-[168px] h-[96px] p-4 rounded-xl cursor-pointer">
+      <img src={iconSrc} alt={altName} />
+      <span>{categoryName}</span>
+    </button>
+  );
+};
 
 const HeroesAndLegends = () => {
   return (
@@ -23,12 +75,26 @@ const HeroesAndLegends = () => {
           </div>
           <div className="absolute inset-0 bg-[rgba(10,11,14,0.5)]"></div>
         </div>
-        <div className="bg-lightgoldcolorfive p-4 rounded-xl">
-          <p>Sort by category</p>
+        <div className="bg-lightgoldcolorsix p-4 rounded-xl">
+          <p className="mb-4">Sort by category</p>
+          <div className="grid grid-cols-5">
+            {HEROES_CATEGORIES.map((cat) => (
+              <HeroesCategory
+                categoryName={cat.categoryName}
+                iconSrc={monarchIcon}
+                altName={cat.altName}
+              />
+            ))}
+          </div>
+        </div>
+        <div>
+          <h2 className="text-xl sm:text-4xl lg:text-3xl font-bold">
+            Top Picks
+          </h2>
         </div>
       </div>
     </section>
   );
-}
+};
 
 export default HeroesAndLegends;
