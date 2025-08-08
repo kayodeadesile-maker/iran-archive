b76843d95657b6b0dd914cc9c39a18abbe9cc61a
DESKTOP-8AR3IAJ\USER PC <busariroqeeb16@gmail.com>
2025-08-08 17:00:20 +0100

    feat: hero section updated

--- src/pages/explore-archive/heroes-and-legends/heroes-and-legends.tsx
+++ src/pages/explore-archive/heroes-and-legends/heroes-and-legends.tsx
@@ -112,7 +112,7 @@ const HeroesAndLegends = () => {
             title="proverbs cover image"
             className="h-full w-full absolute inset-0 object-cover"
           />
-          <div className="flex flex-col min-h-[250px] lg:min-h-[340px] justify-center relative z-10 p-4 xl:p-6">
+          <div className="flex flex-col min-h-[250px] lg:min-h-[340px] justify-center relative z-10 p-4 sm:p-6 xl:p-8">
             <h1 className="text-xl sm:text-4xl lg:text-5xl font-inter font-bold text-white mb-1 auto-translate leading-xs-normal">
               Reincarnating the Dead,
               <br /> Celebrating Living Legends.
